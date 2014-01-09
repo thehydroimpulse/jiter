bcb7cd81b7c2f1488dcddaff86aac7cd27d2fe37
Daniel Fagnan <dnfagnan@gmail.com>
2014-01-09 02:20:17 -0700

    got it finally working. No more bus errors.
    Signed-off-by: Daniel Fagnan <dnfagnan@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,18 @@
-#[crate_id = "jiter#0.0.1"]
-#[desc = "Jiter"]
-#[license = "MIT"]
-#[crate_type = "bin"]
-#[feature(globs)]
+#[crate_id = "jiter#0.0.1"];
+#[desc = "Jiter"];
+#[crate_type = "bin"];
+#[license = "MIT"];
+#[warn(non_camel_case_types)];
+#[feature(globs)];
 
 use std::ptr;
-use std::libc::{c_char, c_int, size_t, off_t, c_void, mmap, PROT_EXEC};
+use std::libc::{c_char, size_t, c_void, PROT_EXEC};
 use std::libc;
 use std::os;
-use std::vec;
-use std::println;
 use std::cast;
+
+#[no_mangle]
+type JitFn = extern "C" fn(n: int) -> int;
 
 pub mod raw {
     use std::libc;
@@ -20,95 +22,109 @@ pub mod raw {
                 fd   : libc::c_int,   offset : libc::off_t) -> *u8;
         pub fn munmap(addr : *u8, length : libc::size_t) -> libc::c_int;
         pub fn mprotect(addr: *libc::c_char, length: libc::size_t, prot: libc::c_int) -> libc::c_int;
+        pub fn memcpy(dest: *libc::c_void, src: *libc::c_void, n: libc::size_t) -> *libc::c_void;
     }
 
-    /* From /usr/include/asm-generic/mman-common.h on Linux */
-
-    /* prot values */
     pub static PROT_NONE   : libc::c_int = 0x0;
     pub static PROT_READ   : libc::c_int = 0x1;
     pub static PROT_WRITE  : libc::c_int = 0x2;
     pub static PROT_EXEC   : libc::c_int = 0x4;
-    // ...
 
-    /* flags */
     pub static MAP_SHARED  : libc::c_int = 0x1;
     pub static MAP_PRIVATE : libc::c_int = 0x2;
-    // ...
 }
 
 struct MappedRegion {
-  addr: *u8,
-  len: libc::size_t
+    addr: *u8,
+    len: libc::size_t
 }
 
 impl std::fmt::Default for MappedRegion {
-  fn fmt(value: &MappedRegion, f: &mut std::fmt::Formatter) {
-    write!(f.buf, "MappedRegion\\{{}, {}\\}", value.addr, value.len);
-  }
+    fn fmt(value: &MappedRegion, f: &mut std::fmt::Formatter) {
+        write!(f.buf, "MappedRegion\\{{}, {}\\}", value.addr, value.len);
+    }
 }
 
 impl Drop for MappedRegion {
-  #[inline(never)]
-  fn drop(&mut self) {
-    unsafe {
-      if raw::munmap(self.addr, self.len) < 0 {
-        fail!(format!("munmap({}, {}): {}", self.addr, self.len, os::last_os_error()));
-      }
+    #[inline(never)]
+    fn drop(&mut self) {
+        unsafe {
+            if raw::munmap(self.addr, self.len) < 0 {
+                fail!(format!("munmap({}, {}): {}", self.addr, self.len, os::last_os_error()));
+            }
+        }
     }
-  }
 }
 
 #[inline(never)]
 unsafe fn mmap(size: size_t) -> Result<MappedRegion, ~str> {
-  let buf = raw::mmap(0 as *libc::c_char, size,
-    libc::PROT_READ | libc::PROT_WRITE,
-    libc::MAP_PRIVATE | libc::MAP_ANON,
-    -1, 0);
-  return if buf == -1 as *u8 {
-    Err(os::last_os_error())
-  } else {
-    Ok(MappedRegion{ addr: buf, len: size })
-  }
+    let buf = raw::mmap(0 as *libc::c_char, size,
+        libc::PROT_READ | libc::PROT_WRITE,
+        libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0);
+
+    return if buf == -1 as *u8 {
+        Err(os::last_os_error())
+    } else {
+        Ok(MappedRegion{ addr: buf, len: size })
+    }
 }
 
 pub unsafe fn make_mem_exec(m: *u8, size: size_t) -> int {
-  if raw::mprotect(m as *libc::c_char, size, libc::PROT_READ | PROT_EXEC) == -1 {
-    fail!("err: mprotect");
-  }
+    if raw::mprotect(m as *libc::c_char, size, libc::PROT_READ | PROT_EXEC) == -1 {
+        fail!("err: mprotect");
+    }
 
-  return 0;
+    return 0;
 }
 
 // Guessing the bus error is here.
-pub unsafe fn emit_code(src: &[u8], mem: &MappedRegion) {
-  ptr::copy_memory(mem.addr as *mut c_void, src.as_ptr() as *mut c_void, src.len());
+pub unsafe fn emit_code(src: *u8, len: uint, mem: &MappedRegion) {
+    ptr::copy_memory(mem.addr as *mut c_void, src as *mut c_void, len);
 }
 
 fn main() {
 
-  // 0:  c6 03 02  mov    BYTE PTR [ebx],2
-  // 3:  83 c3 05  add    ebx,5
   let code = [
-  0xC6, 0x00, 0x05, 0x83, 0xC0, 0x0A, 0xC3
+    0x48, 0x89, 0xf8,       // mov %rdi, %rax
+    0x48, 0x83, 0xc0, 0x04, // add $4, %rax
+    0xc3                    // ret
   ];
 
   unsafe {
 
-    let m = match mmap(1024) {
-      Ok(r) => r,
-      Err(s) => fail!("err: {}", s)
-    };
+        //let m = match mmap(1024) {
+            // Ok(r) => r,
+            // Err(s) => fail!("err: {}", s)
+        //};
 
-    emit_code(code, &m);
-    make_mem_exec(m.addr, m.len);
+        let buf = raw::mmap(0 as *libc::c_char, code.len() as u64, libc::PROT_READ | libc::PROT_WRITE,
+            libc::MAP_PRIVATE | libc::MAP_ANON, -1, 0);
 
-    let JITFn: extern "C" fn(num: u32) -> u32 =
-      cast::transmute(m.addr);
-    //JITFn = m.addr as |num: u32| -> u32;
-    println!("Function: {}", m);
-    let result = JITFn(2);
-    println!("result: {}", result as int);
-    println("Works");
-  }
+        if buf == -1 as *u8 {
+            fail!(os::last_os_error());
+        }
+
+        println("copying the memory contents.");
+        raw::memcpy(buf as * c_void, code.as_ptr() as *c_void, code.len() as size_t);
+
+        println!("original: {} mmapped: {}", *(code.as_ptr()), *buf);
+
+        // Check the mmapped region contains the exact correct contents.
+        assert!(*(code.as_ptr()) == *buf);
+
+        println("protecting the mmapped region.");
+        if raw::mprotect(buf as *libc::c_char, code.len() as u64, libc::PROT_READ | PROT_EXEC) == -1 {
+            fail!("err: mprotect");
+        }
+
+        //emit_code(code.as_ptr(), code.len(), &m);
+        //make_mem_exec(m.addr, m.len);
+
+        let func: JitFn = cast::transmute(buf);
+        //JITFn = m.addr as |num: u32| -> u32;
+        //println!("Function: {}", m);
+        let value = func(5);
+        //println!("result: {}", result);
+        println!("func(): {}", value);
+    }
 }
