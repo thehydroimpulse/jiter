ff059c0b300db4373b1a8d7d8ee97b2ceb313dda
Daniel Fagnan <dnfagnan@gmail.com>
2014-11-22 11:52:23 -0700

    Cargo + Rust nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,19 @@
-#[crate_id = "jiter#0.0.1"];
-#[desc = "Jiter"];
-#[crate_type = "lib"];
-#[license = "MIT"];
+extern crate libc;
 
-//! Jitter allows you to dynamically compile machine code during runtime. This allows you
-//! to speed up certain operations, in some cases.
-//!
-//! This library is fairly low-level, and currently doesn't support any helper functions
-//! to produce assembly code.
-
-use std::cast;
+use std::mem;
 use std::ptr;
-use std::libc;
-use std::os::{MemoryMap,MapReadable,MapWritable};
+use std::os::{MemoryMap, MapReadable, MapWritable};
+
+unsafe fn transmute_harder<T, U>(from: T) -> U {
+    let mut to: U = mem::uninitialized();
+    // copy_memory and forget can't trigger failure, so `from` and `to` won't have destructor run
+    // extraneously
+    let p = &mut to as *mut U;
+    *p = ptr::read(&from as *const _ as *const U);
+    ptr::copy_memory(&mut to as *mut U, &from as *const _ as *const U, 1);
+    mem::forget(from);
+    to
+}
 
 /// A function type in the context of a JIT-compiler.
 pub struct Function<T> {
@@ -40,17 +41,17 @@ impl<T> Function<T> {
 /// (or during) this function.
 fn jit_func<T>(region: &MemoryMap, contents: &[u8]) -> T {
     unsafe {
-        ptr::copy_memory(region.data, contents.as_ptr(), region.len);
-        libc::mprotect(region.data as *libc::c_void, 
-                       region.len as libc::size_t, 
+        ptr::copy_memory(region.data(), contents.as_ptr(), region.len());
+        libc::mprotect(region.data() as *mut libc::c_void, 
+                       region.len() as libc::size_t, 
                        libc::PROT_EXEC | libc::PROT_READ as libc::c_int);
 
         for i in std::iter::range(0, contents.len()) {
             assert_eq!(*(contents.as_ptr().offset(i as int)),
-                       *region.data.offset(i as int));
+                       *region.data().offset(i as int));
         }
 
-        cast::transmute(region.data)
+        transmute_harder(region.data())
     }
 }
 
@@ -65,10 +66,10 @@ fn test_jit_func() {
 
     let region = match MemoryMap::new(contents.len(), &[MapReadable, MapWritable]) {
         Ok(map) => map,
-        Err(err) => fail!(err)
+        Err(err) => panic!(err)
     };
 
-    let Add = jit_func::<extern "C" fn(int) -> int>(&region, contents);
+    let Add = jit_func::<extern "C" fn(int) -> int>(&region, contents.as_slice());
     assert_eq!(Add(4), 8);
     println!("value: {}", Add(4));
 }
